8e6afdbd6ca28a524edaa224364cefa39d66dcd8
thoriqib <thoriq312@gmail.com>
2026-03-02 09:42:12 +0700

    update link

--- resources/views/layouts/adminlte.blade.php
+++ resources/views/layouts/adminlte.blade.php
@@ -107,7 +107,7 @@
                         <ul class="nav nav-treeview">
 
                             <li class="nav-item">
-                                <a href="https://sites.google.com/view/mr1500/risiko-strategis-2026"
+                                    <a href="http://s.bps.go.id/1571_MR2026"
                                 class="nav-link">
                                     <i class="far fa-circle nav-icon"></i>
                                     <p>Lembar Kerja Manajemen Risiko BPS Kota Jambi</p>
@@ -115,8 +115,9 @@
                                 </a>
                             </li>
                             <li class="nav-item">
-                                <a href="http://s.bps.go.id/1571_MR2026"
+                                <a href="https://sites.google.com/view/mr1500/risiko-strategis-2026"
                                 class="nav-link">
+                                
                                     <i class="far fa-circle nav-icon"></i>
                                     <p>Rujukan BPS Provinsi Jambi</p>
                                 </a>
